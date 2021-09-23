645c0b7280f068039663030d6cafc2296e9e35d4
Md Joynal Abdin Robel <robel.joynal@gmail.com>
2021-09-23 17:48:19 +0600

    finished

--- main.js
+++ main.js
@@ -2,9 +2,15 @@
 const searchBook = () => {
   const inputField = document.getElementById("search-field").value;
   document.getElementById("search-field").value = "";
+
   console.log(inputField);
   loadData(inputField);
+  spinnerLoading();
 };
+// Spinner
+const spinnerLoading = ()=>{
+  document.getElementById("spinner").style.display = "block";
+} 
 
 // load data function
 
@@ -21,31 +27,36 @@ const displayData = (books) => {
   console.log(books);
   console.log(books.length);
   const search = document.getElementById("search")
-  search.innerText = "there are available search results" +  books.length;
+  search.innerText = "There available search results are : " +  books.length;
   const searchResult = document.getElementById("search-result");
-  searchResult.innerHTML = "";
   if (books.length === 0 || "") {
     document.getElementById("error-message").style.display = "block";
   }
-  books.forEach((book) => {
-    const div = document.createElement("div");
-    div.classList.add("col");
-  const img='https://covers.openlibrary.org/b/id/'+book.cover_i+'-M.jpg';
+  else{
+    books.forEach((book) => {
+      const div = document.createElement("div");
+      div.classList.add("col");
+    const img='https://covers.openlibrary.org/b/id/'+book.cover_i+'-M.jpg';
+  
+      div.innerHTML = `
+      
+  
+              <div class="card-body">
+              <img class="img-fluid" src='${img}'/>
+                <h4>${book.title.slice(0, 9)}</h4>
+                <p class="bg-primary">${book.author_name}</p>
+                <p>${book.publisher}</p>
+  
+                <h5>${book.first_publish_year}</h5>
+              </div>
+      `;
+      searchResult.appendChild(div);
+    });
+  }
+  
+  document.getElementById("spinner").style.display = "none"
+  // document.getElementById("error-message").style.display = "none"
 
-    div.innerHTML = `
-    
-
-            <div class="card-body">
-            <img class="img-fluid" src='${img}'/>
-              <h4>${book.title.slice(0, 9)}</h4>
-              <p class="bg-primary">${book.author_name}</p>
-              <p>${book.publisher}</p>
-
-              <h5>${book.first_publish_year}</h5>
-            </div>
-    `;
-    searchResult.appendChild(div);
-  });
 
 };
 
